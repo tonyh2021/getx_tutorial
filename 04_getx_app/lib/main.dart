42cf1495360db4628a09fdb176d734d1590a76c1
Tony <tong.decula@gmail.com>
2023-04-18 23:45:47 -0400

    feat: add asset path and home page.

--- 04_getx_app/lib/main.dart
+++ 04_getx_app/lib/main.dart
@@ -14,14 +14,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
-      home: Container(color: Colors.white,),
+      initialRoute: "/",
+      getPages: [
+        GetPage(name: "/", page: () => MyHomePage()),
+      ],
     );
   }
 }
