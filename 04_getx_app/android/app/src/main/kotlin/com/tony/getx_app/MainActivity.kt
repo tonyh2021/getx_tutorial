c0be3e41f7187436c14ad5bd488d4d54e36b4809
Tony <tong.decula@gmail.com>
2023-04-18 23:42:06 -0400

    feat: add new sample

---
+++ 04_getx_app/android/app/src/main/kotlin/com/tony/getx_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.tony.getx_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 04_getx_app/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'content_page.dart';
+import 'my_detail_page.dart';
+import 'my_home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(color: Colors.white,),
+    );
+  }
+}
